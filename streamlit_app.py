e9e6478a746e2e4d9f3e18db946c82cffe5fe97a
laurentindra <laurentindra21@gmail.com>
2025-02-11 13:27:31 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,151 +1,55 @@
-import streamlit as st
+
+
 import pandas as pd
-import math
-from pathlib import Path
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal
+import streamlit as st
 
-# Set the title and favicon that appear in the Browser's tab bar.
-st.set_page_config(
-    page_title='GDP dashboard',
-    page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
-)
+# Streamlit app title
+st.title('Signal Analysis with Spectrogram and Histogram')
 
-# -----------------------------------------------------------------------------
-# Declare some useful functions.
+# File upload widget
+uploaded_file = st.file_uploader("Upload CSV File", type="csv")
 
-@st.cache_data
-def get_gdp_data():
-    """Grab GDP data from a CSV file.
+if uploaded_file is not None:
+    # Reading the uploaded data file
+    df = pd.read_csv(uploaded_file, header=None)
 
-    This uses caching to avoid having to read the file every time. If we were
-    reading from an HTTP endpoint instead of a file, it's a good idea to set
-    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
-    """
+    # Plotting Line Plots for each Channel (3 channels)
+    st.subheader('Line Plot for Each Channel')
+    for channel_idx in range(df.shape[0]):
+        fig, ax = plt.subplots(figsize=(10, 3))
+        ax.plot(df.columns, df.iloc[channel_idx, :], label=f'Channel {channel_idx+1}')
+        ax.set_title(f'Signal for Channel {channel_idx+1}')
+        ax.set_xlabel('Sample Index')
+        ax.set_ylabel('Amplitude')
+        ax.legend()
+        st.pyplot(fig)
 
-    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent/'data/gdp_data.csv'
-    raw_gdp_df = pd.read_csv(DATA_FILENAME)
+    # Spectrogram for each channel (using scipy.signal.spectrogram)
+    fs = 16000  # Assume sampling rate, adjust as per your data
 
-    MIN_YEAR = 1960
-    MAX_YEAR = 2022
+    st.subheader('Spectrogram for Each Channel')
+    for channel_idx in range(df.shape[0]):
+        data_signal = df.iloc[channel_idx, :].values  # Convert to numpy array
+        f, t, Sxx = signal.spectrogram(data_signal, fs=fs, nperseg=256)
 
-    # The data above has columns like:
-    # - Country Name
-    # - Country Code
-    # - [Stuff I don't care about]
-    # - GDP for 1960
-    # - GDP for 1961
-    # - GDP for 1962
-    # - ...
-    # - GDP for 2022
-    #
-    # ...but I want this instead:
-    # - Country Name
-    # - Country Code
-    # - Year
-    # - GDP
-    #
-    # So let's pivot all those year-columns into two: Year and GDP
-    gdp_df = raw_gdp_df.melt(
-        ['Country Code'],
-        [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
-        'Year',
-        'GDP',
-    )
+        fig, ax = plt.subplots(figsize=(8, 4))
+        ax.pcolormesh(t, f, Sxx, shading='gouraud')
+        ax.set_title(f'Spectrogram for Channel {channel_idx+1}')
+        ax.set_ylabel('Frequency [Hz]')
+        ax.set_xlabel('Time [s]')
+        fig.colorbar(ax.pcolormesh(t, f, Sxx, shading='gouraud'), ax=ax, label='Intensity')
+        st.pyplot(fig)
 
-    # Convert years from string to integers
-    gdp_df['Year'] = pd.to_numeric(gdp_df['Year'])
+    # Histogram of Amplitude Distribution for each channel
+    st.subheader('Amplitude Histogram for Each Channel')
+    for channel_idx in range(df.shape[0]):
+        fig, ax = plt.subplots(figsize=(6, 3))
+        df.iloc[channel_idx, :].plot(kind='hist', bins=30, alpha=0.7, ax=ax)
+        ax.set_title(f'Amplitude Histogram for Channel {channel_idx+1}')
+        ax.set_xlabel('Amplitude')
+        ax.set_ylabel('Frequency')
+        st.pyplot(fig)
 
-    return gdp_df
-
-gdp_df = get_gdp_data()
-
-# -----------------------------------------------------------------------------
-# Draw the actual page
-
-# Set the title that appears at the top of the page.
-'''
-# :earth_americas: GDP dashboard
-
-Browse GDP data from the [World Bank Open Data](https://data.worldbank.org/) website. As you'll
-notice, the data only goes to 2022 right now, and datapoints for certain years are often missing.
-But it's otherwise a great (and did I mention _free_?) source of data.
-'''
-
-# Add some spacing
-''
-''
-
-min_value = gdp_df['Year'].min()
-max_value = gdp_df['Year'].max()
-
-from_year, to_year = st.slider(
-    'Which years are you interested in?',
-    min_value=min_value,
-    max_value=max_value,
-    value=[min_value, max_value])
-
-countries = gdp_df['Country Code'].unique()
-
-if not len(countries):
-    st.warning("Select at least one country")
-
-selected_countries = st.multiselect(
-    'Which countries would you like to view?',
-    countries,
-    ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
-
-''
-''
-''
-
-# Filter the data
-filtered_gdp_df = gdp_df[
-    (gdp_df['Country Code'].isin(selected_countries))
-    & (gdp_df['Year'] <= to_year)
-    & (from_year <= gdp_df['Year'])
-]
-
-st.header('GDP over time', divider='gray')
-
-''
-
-st.line_chart(
-    filtered_gdp_df,
-    x='Year',
-    y='GDP',
-    color='Country Code',
-)
-
-''
-''
-
-
-first_year = gdp_df[gdp_df['Year'] == from_year]
-last_year = gdp_df[gdp_df['Year'] == to_year]
-
-st.header(f'GDP in {to_year}', divider='gray')
-
-''
-
-cols = st.columns(4)
-
-for i, country in enumerate(selected_countries):
-    col = cols[i % len(cols)]
-
-    with col:
-        first_gdp = first_year[first_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-        last_gdp = last_year[last_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-
-        if math.isnan(first_gdp):
-            growth = 'n/a'
-            delta_color = 'off'
-        else:
-            growth = f'{last_gdp / first_gdp:,.2f}x'
-            delta_color = 'normal'
-
-        st.metric(
-            label=f'{country} GDP',
-            value=f'{last_gdp:,.0f}B',
-            delta=growth,
-            delta_color=delta_color
-        )
